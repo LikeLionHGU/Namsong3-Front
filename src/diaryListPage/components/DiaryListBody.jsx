1276f55415c7b1646ae248189a351b9d8f6f7026
ejPark43 <pej22200310@handong.ac.kr>
2024-07-21 01:38:59 +0900

    feat: 달력부분 제외 일지목록 페이지 간단 완성, mui 설치 후 아이콘 사용

--- src/diaryListPage/components/DiaryListBody.jsx
+++ src/diaryListPage/components/DiaryListBody.jsx
@@ -1,48 +1,39 @@
 import React from "react";
-
 import styled from "styled-components";
-import dummy from "../../db/data.json";
+import Diaries from "./Diaries";
+import GoalCard from "./GoalCard";
+
 function DiaryListBody() {
   return (
     <Container>
       <CenterBox>
         <ContentWrapper>
-          {" "}
-          {/* 필요없으면 나중에 삭제해도 될지도...  */}
+          {/* ContentWrapper는 나중에 삭제해도 될지도...  */}
           <SidePart>
-            <GoalCard></GoalCard>
-            <CalendarCard></CalendarCard>
+            <GoalCardBox>
+              {/* GoalCardBox: 목표 카드(왼쪽 상단부분위치) */}
+              <GoalCard />
+            </GoalCardBox>
+            <CalendarCard>
+              달력 들어갈 부분
+              {/* CalendarCard: 달력 들어갈 부분 */}
+            </CalendarCard>
           </SidePart>
           <ListPart>
-            <Searchbar></Searchbar>
+            <Diaries />
+            {/* <Searchbar
+              placeholder="제목+내용을 입력해보세요.
+              "
+            ></Searchbar>
             <DairyListBox>
               <div className="diary-list-head">
                 <div className="diary-add">일지 추가하기</div>
                 <div className="diary-dropdown">드롭다운</div>
               </div>
               <DiaryList>
-                {dummy.goals.map((goal, index) => (
-                  <Diary key={index}>
-                    <div className="diary-title-date">
-                      <div className="diary-title">{goal.title}</div>
-                      <div className="diary-date">
-                        {/* <div className="diary-start-date">
-                          {goal.startDate} ~{" "}
-                        </div> */}
-                        <div className="diary-end-date">{goal.dueDate}</div>
-                      </div>
-                    </div>
-                    {goal.imgUrl ? (
-                      <Image
-                        style={{ backgroundImage: `url(${goal.imgUrl})` }}
-                      />
-                    ) : (
-                      <></>
-                    )}
-                  </Diary>
-                ))}
+                <Diaries />
               </DiaryList>
-            </DairyListBox>
+            </DairyListBox> */}
           </ListPart>
         </ContentWrapper>
       </CenterBox>
@@ -57,112 +48,44 @@ const Container = styled.div`
   justify-content: center;
   width: 100%; // !! 나중에 변경해야할 부분.
   height: 100%;
-  border: 2px solid red;
+  /* border: 2px solid red; */
 `;
 const CenterBox = styled.div`
   display: flex;
   justify-content: center;
   width: 60%; //
-  border: 2px solid black;
+  /* border: 2px solid black; */
 `;
 const ContentWrapper = styled.div`
   // 필요없으면 나중에 삭제해도 될지도...
   display: flex;
-  border: 2px solid purple;
+  border: 2px solid gray;
   width: 100%;
 `;
 
 const SidePart = styled.div`
   display: flex;
   flex-direction: column;
-  border: 2px solid blue;
+  /* border: 2px solid blue; */
   width: 40%;
 `;
-const GoalCard = styled.div`
+const GoalCardBox = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: start; // 이거 지우면 목표 카드가 위아래 중앙으로 오게됨
   height: 320px;
-  border: 2px solid gold;
+  /* border: 2px solid gold; */
 `;
 const CalendarCard = styled.div`
   display: flex;
   height: 300px;
+  justify-content: center;
+  align-items: center;
   border: 2px solid red;
 `;
 const ListPart = styled.div`
   display: flex;
   flex-direction: column;
-  border: 2px solid cyan;
+  /* border: 2px solid cyan; */
   width: 60%;
-`;
-const Searchbar = styled.div`
-  display: flex;
-  border: 2px solid red;
-  margin: 10px;
-  height: 40px;
-`;
-const DairyListBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  /* border: 2px solid lime; */
-  height: 100%;
-
-  .diary-list-head {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-
-    .diary-add {
-      cursor: pointer;
-      border: 2px solid red;
-    }
-    .diary-dropdown {
-      border: 2px solid red;
-    }
-  }
-`;
-
-const DiaryList = styled.div`
-  display: flex;
-  height: 530px;
-  flex-direction: column;
-  border: 2px solid blue;
-  overflow: auto;
-`;
-const Diary = styled.div`
-  display: flex;
-  justify-content: space-between;
-  flex-direction: row;
-  margin-bottom: 20px;
-  align-items: center;
-  border: 1px solid lightgray;
-  background-color: lightgray;
-  .diary-title-date {
-    display: flex;
-    /* border: 2px solid red; */
-    flex-direction: column;
-    padding-left: 20px;
-    margin-top: 20px;
-    margin-bottom: 20px;
-    font-size: 15px;
-    .diary-title {
-      /* border: 2px solid black; */
-      font-weight: bold;
-    }
-    .diary-date {
-      .diary-end-date {
-        color: gray;
-        margin-top: 5px;
-        font-size: 13px;
-        /* border: 2px solid purple; */
-      }
-    }
-  }
-`;
-const Image = styled.div`
-  /* border: 2px solid red; */
-  display: flex;
-  height: 100px;
-  width: 130px;
-  background-size: cover;
-  background-position: center;
 `;
