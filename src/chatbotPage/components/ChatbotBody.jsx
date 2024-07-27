0b5c1bd391ef046f55cb103b8b2c82b976e4ad5d
ejPark43 <pej22200310@handong.ac.kr>
2024-07-27 20:18:53 +0900

    feat: 챗봇 대화창 페이지 ui 간단 구현

--- src/chatbotPage/components/ChatbotBody.jsx
+++ src/chatbotPage/components/ChatbotBody.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import dummy from "../../db/data.json";
-
+import sendIcon from "../../asset/Icon/send.svg";
 function ChatbotBody() {
   //   let botTexts = dummy.Chatbot;
 
@@ -9,20 +9,37 @@ function ChatbotBody() {
     <div>
       <PageWrapper>
         <CenterBox className="flex">
-          <Chatbot className="flex">
+          <Chattings>
+            <Chatbot className="flex">
+              <ChatbotIcon className="flex" />
+              {dummy.chatbot.map((texts, index) => (
+                <ChatbotText key={index}>{texts.text}</ChatbotText>
+                //   <ChatbotText key={index}>{texts.text}</ChatbotText>
+              ))}
+            </Chatbot>
+            <User>
+              {dummy.user.map((texts, index) => (
+                <UserText key={index}>{texts.text}</UserText>
+              ))}
+            </User>
             <ChatbotIcon className="flex" />
-            {dummy.chatbot.map((texts, index) => (
-              <ChatbotText key={index}>{texts.text}</ChatbotText>
-              //   <ChatbotText key={index}>{texts.text}</ChatbotText>
-            ))}
-          </Chatbot>
-          <User>
-            {dummy.user.map((texts, index) => (
-              <UserText key={index}>{texts.text}</UserText>
-            ))}
-          </User>
-          <ChatbotIcon className="flex" />
-          <ChatbotText className="flex"> . . . </ChatbotText>
+            <ChatbotText className="flex"> . . . </ChatbotText>
+          </Chattings>
+          <UserInteractField>
+            <> {/* 일지 작성 조건 갖춰지면 클릭할 부분 보여주기 */}</>
+            <SummarizeArea>
+              {" "}
+              <span>
+                (봇이름)과 대화를 통해 충분히 오늘의 감상을 기록하셨다면{" "}
+                <a href="/chatbot">일지를 요약</a>해보세요!
+              </span>{" "}
+              <span>일지 요약하기 &gt; </span>
+            </SummarizeArea>
+            <TypingBox>
+              <TypingArea placeholder="메시지를 작성해주세요! "></TypingArea>
+              <SendButton src={sendIcon} />
+            </TypingBox>
+          </UserInteractField>
         </CenterBox>
       </PageWrapper>
     </div>
@@ -36,7 +53,7 @@ const PageWrapper = styled.div`
   justify-content: center;
   width: 100%;
   height: 100%;
-  border: 2px solid red;
+  /* border: 2px solid red; */
 
   .flex {
     display: flex;
@@ -44,15 +61,19 @@ const PageWrapper = styled.div`
 `;
 const CenterBox = styled.div`
   flex-direction: column;
-  width: 70%; // << 나중에 바꿔야 할 부분
+  justify-content: space-between;
+
+  width: 792px; // << 나중에 바꿔야 할 부분
   height: 708px;
-  border: 2px solid orange;
+
   padding: 21px 20px;
+  /* border: 2px solid orange; */
+  border: 2px solid lightgray;
 `;
 
 const Chatbot = styled.div`
   flex-direction: column;
-  border: 2px solid gold;
+  /* border: 2px solid gold; */
   width: 70%;
   /* height: 50%; */
 `;
@@ -67,6 +88,15 @@ const ChatbotIcon = styled.div`
   margin-bottom: 12px;
 `;
 
+// 챗봇이랑 유저가 나눈 대화 내용
+const Chattings = styled.div`
+  // 나중에 채팅하다보면 길어질 것 .. 그때는 스크롤로 채팅 기록 확인하도록.
+  display: flex;
+  height: 100%;
+  overflow: auto;
+  flex-direction: column;
+  /* border: 2px solid red; */
+`;
 const ChatbotText = styled.div`
   display: flex;
   background-color: white;
@@ -74,14 +104,17 @@ const ChatbotText = styled.div`
   border-radius: 8px;
   border-top-left-radius: 0px;
   margin-bottom: 8px;
-  border: 2px solid green;
+  /* border: 2px solid green; */
+  border: 2px solid lightgray;
 `;
 
 const User = styled.div`
   align-self: end;
   flex-direction: column;
-  border: 2px solid skyblue;
+  margin-top: 24px;
   width: 70%;
+  /* border: 2px solid skyblue; */
+  /* border: 2px solid lightgray; */
 `;
 
 const UserText = styled.div`
@@ -91,5 +124,64 @@ const UserText = styled.div`
   border-radius: 8px;
   border-top-right-radius: 0px;
   margin-bottom: 8px;
-  border: 2px solid blue;
+  /* border: 2px solid blue; */
+  border: 2px solid lightgray;
+`;
+
+// AI 요약하기 버튼 넣는 부분 + 유저가 타이핑하는 필드
+const UserInteractField = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-self: flex-end; //
+  /* width: 100%; */
+  height: 130px;
+  padding: 20px;
+  /* border: 2px solid purple; */
+`;
+
+const SummarizeArea = styled.div`
+  display: flex;
+  /* width: 100%; */
+  justify-content: space-between;
+  height: 50%;
+  align-items: center;
+  padding-left: 15px;
+  padding-right: 15px;
+  margin-bottom: 5px;
+  /* border: 2px solid blue; */
+  border: 2px solid lightgray;
+`;
+
+const TypingBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 50%;
+  padding-left: 15px;
+  padding-right: 15px;
+  /* border: 2px solid pink; */
+  background-color: #d9d9d9;
+  border: 2px solid lightgray;
+`;
+
+const TypingArea = styled.input`
+  display: flex;
+  /* width: 100%; */
+  height: 90%;
+  width: 94%;
+  outline: none;
+  /* padding-left: 15px; */
+  font-size: 16px;
+  /* border: 2px solid pink; */
+  background-color: #d9d9d9;
+  border: none;
+`;
+const SendButton = styled.img`
+  /* background-color: ; */
+
+  height: 27px;
+  width: 27px;
+  border-radius: 4px;
+  margin-right: 4px;
+  /* border: 2px solid red; */
 `;
