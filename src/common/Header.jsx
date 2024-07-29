839a4cdf680fd7fbcf889946935ddfab3fa16dc4
JongHyun <jonghyun@JongHyuns-MacBook-Pro.local>
2024-07-29 22:00:38 +0900

    style: 헤더 스타일 작업

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import Logo from "../asset/Logo/Logo.svg";
 
 const Wrapper = styled.div`
   width: 100vw;
@@ -8,15 +9,17 @@ const Wrapper = styled.div`
   border-bottom: 1px solid #e0dfde;
   display: flex;
   align-items: center;
+  justify-content: center;
 `;
 
-const LogoImg = styled.div`
-  margin-right: auto;
-  margin-left: 40px;
+const Container = styled.div`
+  width: 792px;
+  display: flex;
+  justify-content: space-between;
 `;
+
+const LogoImg = styled.img``;
 const RightContainer = styled.div`
-  margin-left: auto;
-  margin-right: 40px;
   display: flex;
   font-size: 16px;
   font-weight: 500;
@@ -36,11 +39,13 @@ function Header() {
   return (
     <div>
       <Wrapper>
-        <LogoImg>로고</LogoImg>
-        <RightContainer>
-          <HomeBtn onClick={handleClickHome}>Home</HomeBtn>
-          <ProfileBtn>profile</ProfileBtn>
-        </RightContainer>
+        <Container>
+          <LogoImg src={Logo} alt="" />
+          <RightContainer>
+            <HomeBtn onClick={handleClickHome}>홈화면</HomeBtn>
+            <ProfileBtn>프로필</ProfileBtn>
+          </RightContainer>
+        </Container>
       </Wrapper>
     </div>
   );
