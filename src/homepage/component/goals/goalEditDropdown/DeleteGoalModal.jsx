9de48e119ba685cea4c99bb321a89952c771ec54
ejPark43 <pej22200310@handong.ac.kr>
2024-08-04 00:02:41 +0900

    style: 목표 삭제 모달 스타일링 적용

--- src/homepage/component/goals/goalEditDropdown/DeleteGoalModal.jsx
+++ src/homepage/component/goals/goalEditDropdown/DeleteGoalModal.jsx
@@ -2,113 +2,201 @@ import React from "react";
 import styled from "styled-components";
 
 function DeleteGoalModal({ setIsDeleteModalOpen }) {
-  const closeDeleteGoalModal = (event) => {
+  const closeDeleteGoalModal = () => {
     setIsDeleteModalOpen(false);
-    event.stopPropagation(event); //뒤에 goalclick event 방지용
   };
-  const handleModalClick = (event) => {
-    event.stopPropagation(event); //뒤에 goalclick event 방지용
-  };
+  const handleDeleteGoal = () => {
+    /* 목표 삭제하는 로직 필요*/
 
+    setIsDeleteModalOpen(false);
+  };
   return (
     <div>
-      <Modal>
-        <Overlay onClick={(event) => closeDeleteGoalModal(event)} />
-        <Wrapper onClick={(event) => handleModalClick(event)}>
-          <MainText>목표를 삭제하시겠어요?</MainText>
-          <SubTextContainer>
-            <SubText>삭제한 목표는 복구가 불가능합니다.</SubText>
-            <SubText>목표를 삭제하시겠습니까?</SubText>
-          </SubTextContainer>
-          <ButtonContainer>
-            <Cancel onClick={(event) => closeDeleteGoalModal(event)}>
-              취소
-            </Cancel>
-            <Confirm onClick={(event) => closeDeleteGoalModal(event)}>
-              확인
-            </Confirm>
-          </ButtonContainer>
+      <ModalBackground>
+        <Overlay onClick={closeDeleteGoalModal} />
+        <Wrapper>
+          <h3>목표를 삭제하시겠어요?</h3>
+          <div className="complete-content">
+            삭제한 목표는 복구가 불가능합니다.
+            <br />
+            목표를 삭제하시겠습니까?
+          </div>
+          <Buttons>
+            <CancelBtn>
+              <button onClick={closeDeleteGoalModal}>취소 </button>
+            </CancelBtn>
+            <CompleteBtn>
+              <button onClick={handleDeleteGoal}>확인</button>
+            </CompleteBtn>
+          </Buttons>
         </Wrapper>
-      </Modal>
+        {/* </Overlay> */}
+      </ModalBackground>
     </div>
   );
 }
 
 export default DeleteGoalModal;
 
-const modalStyles = `
-  width: 100vw;
+const modalBase = `
+width: 100vw;
   height: 100vh;
   top: 0;
   left: 0;
   right: 0;
   bottom: 0;
   position: fixed;
-`;
+  `;
 
-const Modal = styled.div`
-  ${modalStyles}
-  z-index: 3;
-`;
+const ModalBackground = styled.div`
+  ${modalBase}
 
+  background: rgba(0, 0, 0, 0.2);
+  z-index: 4;
+  cursor: default;
+`;
 const Overlay = styled.div`
-  ${modalStyles}
-  background: rgba(158, 158, 158, 0.8);
-  cursor: pointer;
+  ${modalBase}
+  cursor: default;
 `;
 
 const Wrapper = styled.div`
-  position: fixed; /* fixed로 변경 */
+  display: flex;
+  flex-direction: column;
+  /* justify-content: center; */
+  align-items: center;
+  position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   background: #ffffff;
-  width: 350px;
-  height: 176px;
+  width: 350px; // 로딩모달 크기
+  height: 180px;
+  border-radius: 12px;
+  padding-top: 5px;
+  z-index: 5;
+  .complete-content {
+    width: 310px;
+    /* height: 100px; */
+    font-size: 15px;
+    text-align: center;
+  }
+`;
+const CancelBtn = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
+  > button {
+    background-color: white !important;
+    color: #6c6c6c !important;
+    border: 1.5px solid #dfdfdf !important;
+    margin-right: 4px;
+
+    &:active {
+      background-color: #f5f5f5 !important;
+    }
+  }
 `;
 
-const MainText = styled.div`
-  font-size: 18px;
-  font-weight: bolder;
-  margin-top: 18px;
-`;
-
-const SubTextContainer = styled.div`
-  margin-top: 5px;
+const Buttons = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const SubText = styled.div`
-  font-size: 16px;
-  margin-top: 3px;
-`;
-
-const ButtonContainer = styled.div`
+  flex-direction: row;
   margin-top: 16px;
+  > div > button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    outline: none;
+    border: none;
+    border-radius: 8px;
+    width: 96px;
+    height: 37px;
+    font-size: 14px;
+    font-weight: bold;
+    color: white;
+    background-color: #798bff;
+    cursor: pointer;
+    margin-left: 4px;
+    &:active {
+      background-color: #586eff;
+    }
+  }
+`;
+const CompleteBtn = styled.div`
   display: flex;
 `;
 
-const Cancel = styled.div`
-  width: 100px;
-  height: 40px;
-  margin-right: 4px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: 1px solid lightgray;
-`;
+// const modalStyles = `
+//   width: 100vw;
+//   height: 100vh;
+//   top: 0;
+//   left: 0;
+//   right: 0;
+//   bottom: 0;
+//   position: fixed;
+// `;
 
-const Confirm = styled.div`
-  width: 100px;
-  height: 40px;
-  margin-left: 4px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: lightgray;
-`;
+// const Modal = styled.div`
+//   ${modalStyles}
+//   z-index: 3;
+// `;
+
+// const Overlay = styled.div`
+//   ${modalStyles}
+//   background: rgba(158, 158, 158, 0.8);
+//   cursor: pointer;
+// `;
+
+// const Wrapper = styled.div`
+//   position: fixed; /* fixed로 변경 */
+//   top: 50%;
+//   left: 50%;
+//   transform: translate(-50%, -50%);
+//   background: #ffffff;
+//   width: 350px;
+//   height: 176px;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+// `;
+
+// const MainText = styled.div`
+//   font-size: 18px;
+//   font-weight: bolder;
+//   margin-top: 18px;
+// `;
+
+// const SubTextContainer = styled.div`
+//   margin-top: 5px;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+// `;
+
+// const SubText = styled.div`
+//   font-size: 16px;
+//   margin-top: 3px;
+// `;
+
+// const ButtonContainer = styled.div`
+//   margin-top: 16px;
+//   display: flex;
+// `;
+
+// const Cancel = styled.div`
+//   width: 100px;
+//   height: 40px;
+//   margin-right: 4px;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   border: 1px solid lightgray;
+// `;
+
+// const Confirm = styled.div`
+//   width: 100px;
+//   height: 40px;
+//   margin-left: 4px;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   background-color: lightgray;
+// `;
